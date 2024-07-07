d26f923eeb5ae26b5525e5e820aceaf5e75fb5f1
Zuhed Shaikh <zuhed95@gmail.com>
2024-07-07 21:33:22 +0530

    add: Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
+import { cn } from "@/libs/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { Navbar } from "@/components/global";
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Zeddit",
-  description: "Post, React, Share"
+  description: "Post, React, Share",
 };
 
 export default function RootLayout({
@@ -16,7 +20,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "bg-slate-50 pt-12 text-slate-900 antialiased min-h-screen",
+          inter.className
+        )}
+      >
+        <header>
+          <Navbar />
+        </header>
+
+        <main className="pt-12 container max-w-7xl mx-auto h-full">
+          {children}
+        </main>
+
+        <Toaster />
+      </body>
     </html>
   );
 }
