069fe9352fd34a1bf17ac50d99b5f5ee091cf792
Zuhed Shaikh <zuhed95@gmail.com>
2024-07-09 08:53:15 +0530

    add: export UserAccountNav, UserAvatar, CloseInterceptedModal

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,2 +1,6 @@
 export { Icons } from "./icons";
+
 export { default as Navbar } from "./navbar";
+export { default as UserAccountNav } from "./user-account-nav";
+export { default as UserAvatar } from "./user-avatar";
+export { default as CloseInterceptedModal } from "./close-intercepted-modal";
