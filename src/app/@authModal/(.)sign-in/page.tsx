a023e2b16c7f2b6400673791d54b95a0ad7ccd65
Zuhed Shaikh <zuhed95@gmail.com>
2024-07-09 08:52:34 +0530

    add: SignInInterceptedPage

---
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -0,0 +1,18 @@
+import { CloseInterceptedModal } from "@/components/global";
+import { SignInForm } from "@/components/sign-in";
+
+export default async function SignInInterceptedPage() {
+  return (
+    <div className="fixed inset-0 bg-zinc-900/20 z-10">
+      <div className="container flex items-center h-full max-w-lg mx-auto">
+        <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
+          <div className="absolute top-4 right-4">
+            <CloseInterceptedModal />
+          </div>
+
+          <SignInForm />
+        </div>
+      </div>
+    </div>
+  );
+}
