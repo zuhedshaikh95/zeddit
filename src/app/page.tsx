40decb6eaea015ea19d50b0aca9654ef4124c38e
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-21 20:09:08 +0530

    feat: create-community redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import { HomeIcon } from "lucide-react";
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>Hello, topper!</main>;
+  return (
+    <section>
+      <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
+
+      <div className="grid grid-cols-1 py-6 gap-y-4 md:grid-cols-3 md:gap-x-4">
+        {/* TODO: feed */}
+
+        {/* TODO: subzeddit info */}
+        <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+          <div className="bg-emerald-100 px-6 py-4">
+            <p className="font-semibold py-3 flex items-center gap-1.5">
+              <HomeIcon className="w-4 h-4" />
+              Home
+            </p>
+          </div>
+
+          <div className="-m-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+            <div className="flex justify-between gap-x-4 py-3">
+              <p className="text-zinc-500">
+                Your personal Zeddit homepage. Come here to check in with your favorite communities.
+              </p>
+            </div>
+
+            <Link className={buttonVariants({ className: 'w-full mt-4 mb-6' })} href="/z/create-community">
+              Create Community
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
