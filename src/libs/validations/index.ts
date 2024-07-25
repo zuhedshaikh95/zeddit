63bcaee3fe90ae15d83aa9300c8b00ecb3c0ae3c
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-25 09:48:42 +0530

    feat: add postValidator

--- src/libs/validations/index.ts
+++ src/libs/validations/index.ts
@@ -1,9 +1,18 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const subZedditPayloadValidator = z.object({
-    name: z.string().min(3).max(21)
+  name: z.string().min(3).max(21),
 });
 
 export const subZedditSubscriptionValidator = z.object({
-    subZedditId: z.string()
+  subZedditId: z.string(),
+});
+
+export const postValidator = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must be longer than 3 characters" })
+    .max(120, { message: "Title must be at least 128 characters" }),
+  subZedditId: z.string(),
+  content: z.any(),
 });
