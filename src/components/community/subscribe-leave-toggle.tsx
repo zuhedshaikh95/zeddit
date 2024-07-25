eab201ecf550ba477b4ba37c5f32c904294bf8d9
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-25 09:48:25 +0530

    feat: add SubscribeLeaveToggle

---
+++ src/components/community/subscribe-leave-toggle.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+import { Button } from "@/components/ui";
+import { useToast } from "@/hooks";
+import { RouteResponseT } from "@types";
+
+interface Props {
+  subZedditId: string;
+  isSubscribed: boolean;
+}
+
+const SubscribeLeaveToggle: React.FC<Props> = ({ subZedditId, isSubscribed }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const { mutate: toggleSubscription, isPending } = useMutation<string, AxiosError<RouteResponseT>>({
+    mutationFn: async () => {
+      const response = await axios.post<RouteResponseT>("/api/v1/subzeddit/subscribe", { subZedditId });
+
+      toast({
+        title: response.data.data === "unsubscribed" ? "Awwwww!" : "Yay!",
+        description: response.data.message,
+      });
+
+      return response.data.data;
+    },
+
+    onSuccess(data, variables, context) {
+      React.startTransition(() => {
+        router.refresh();
+      });
+    },
+  });
+
+  return isSubscribed ? (
+    <Button className="w-full mt-1 mb-4" isLoading={isPending} onClick={() => toggleSubscription()}>
+      Leave community
+    </Button>
+  ) : (
+    <Button className="w-full mt-1 mb-4" isLoading={isPending} onClick={() => toggleSubscription()}>
+      Join to post
+    </Button>
+  );
+};
+
+export default SubscribeLeaveToggle;
