1a9bf91eefed3984d714e713322719d75cab339c
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-25 09:48:54 +0530

    feat: add PostT

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,11 @@
+import { z } from "zod";
+
+import { postValidator } from "@/libs/validations";
+
 export type RouteResponseT = {
   data: null | any;
   error: boolean;
   message: string;
 };
+
+export type PostT = z.infer<typeof postValidator>;
