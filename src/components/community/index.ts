de729536b48a362e1d790f8669b819a1a5794acb
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-26 08:32:17 +0530

    add export CommunityFeed

--- src/components/community/index.ts
+++ src/components/community/index.ts
@@ -1,2 +1,3 @@
 export { default as MiniCreatePost } from "./mini-create-post";
 export { default as SubscribeLeaveToggle } from "./subscribe-leave-toggle";
+export { default as CommunityFeed } from "./community-feed";
