06ed00ff2d15eae5763a7a94a8ae9c9098ab995a
zuhedshaikh95 <zuhed95@gmail.com>
2024-07-26 08:32:58 +0530

    add ExtendedPostT, fix RouteResponseT

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,19 @@
+import { SubZeddit, User, Vote } from "@prisma/client";
 import { z } from "zod";
 
 import { postValidator } from "@/libs/validations";
 
-export type RouteResponseT = {
-  data: null | any;
+export type RouteResponseT<T> = {
+  data: T | null;
   error: boolean;
   message: string;
 };
 
 export type PostT = z.infer<typeof postValidator>;
+
+export type ExtendedPostT = {
+  subZeddit: SubZeddit;
+  votes: Vote[];
+  author: User;
+  comments: Comment[];
+};
