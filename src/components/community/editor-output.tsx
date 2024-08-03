5ce1ff6d2cb0e87873a0a7878bd509c71cf9b8ff
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-03 23:01:42 +0530

    add: EditorOutput

---
+++ src/components/community/editor-output.tsx
@@ -0,0 +1,45 @@
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import React from "react";
+
+const EditorOutputRenderer = dynamic(async () => (await import("editorjs-react-renderer")).default, { ssr: false });
+
+interface Props {
+  content: any;
+}
+
+const style = {
+  paragraph: {
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+  },
+};
+
+const renderers = {
+  image: CustomImageRenderer,
+  code: CustomCodeRenderer,
+};
+
+const EditorOutput: React.FC<Props> = ({ content }) => {
+  return <EditorOutputRenderer className="text-sm" data={content} style={style} renderers={renderers} />;
+};
+
+function CustomImageRenderer({ data }: any) {
+  const src = data.file.url;
+
+  return (
+    <div className="relative w-full min-h-[15rem]">
+      <Image alt="image" className="object-contain" fill src={src} />
+    </div>
+  );
+}
+
+function CustomCodeRenderer({ data }: any) {
+  return (
+    <pre className="bg-gray-800 rounded-md p-4">
+      <code className="text-gray-100 text-sm">{data.code}</code>
+    </pre>
+  );
+}
+
+export default EditorOutput;
