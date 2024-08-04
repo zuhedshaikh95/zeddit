2bc98ef1d3638234932c7b44d9190718c3066aea
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-04 19:09:53 +0530

    add export CustomFeed, Feed, GeneralFeed

--- src/components/community/index.ts
+++ src/components/community/index.ts
@@ -1,4 +1,6 @@
 export { default as MiniCreatePost } from "./mini-create-post";
 export { default as SubscribeLeaveToggle } from "./subscribe-leave-toggle";
-export { default as CommunityFeed } from "./community-feed";
+export { default as Feed } from "./feed";
 export { default as EditorOutput } from "./editor-output";
+export { default as CustomFeed } from "./custom-feed";
+export { default as GeneralFeed } from "./general-feed";
