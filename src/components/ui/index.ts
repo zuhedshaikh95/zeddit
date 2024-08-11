d2cf0ae55be2c6e449de9a5972c06ebbe583c0a1
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-11 17:53:25 +0530

    add export Label, Textarea

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,7 @@
 export { Button } from "./button";
-export { Input } from './input';
+export { Input } from "./input";
+export { Label } from "./label";
+export { Textarea } from "./textarea";
 
 export * as DropdownMenu from "./dropdown-menu";
 export * as Avatar from "./avatar";
