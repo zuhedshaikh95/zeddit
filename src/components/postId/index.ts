d6871894b8e8a15bc47cdd238a7addc73c55d1bc
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-11 17:53:12 +0530

    add export CommentsSection, PostComment, CreateComment, CommentVotes

---
+++ src/components/postId/index.ts
@@ -0,0 +1,4 @@
+export { default as CommentsSection } from "./comments-section";
+export { default as PostComment } from "./post-comment";
+export { default as CreateComment } from "./create-comment";
+export { default as CommentVotes } from "./comment-votes";
