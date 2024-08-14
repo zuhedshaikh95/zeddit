8317e2f6d3096b170c84e17af4f5086dbc61bd81
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-14 06:29:14 +0530

    feat: search subzeddit route

---
+++ src/app/api/v1/search/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { CustomException } from "@/libs/utils";
+import { db } from "@/libs/db";
+import { SUBZEDDIT_SEARCH_QUERY_LIMIT } from "@/configs";
+
+export async function GET(request: NextRequest) {
+  try {
+    const q = request.nextUrl.searchParams.get("q");
+
+    if (!q) throw new CustomException("Invalid query!", 400);
+
+    const results = await db.subZeddit.findMany({
+      where: {
+        name: {
+          startsWith: q,
+        },
+      },
+      include: {
+        _count: true,
+      },
+      take: SUBZEDDIT_SEARCH_QUERY_LIMIT,
+    });
+
+    return NextResponse.json({ data: results, error: false, message: "success!" });
+  } catch (error: any) {
+    return NextResponse.json(
+      { data: null, error: true, message: error.message },
+      { status: error instanceof CustomException ? error.code : 500 }
+    );
+  }
+}
