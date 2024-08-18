95113dce83a29c0802147936cd0b6aa5e74e4508
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-18 22:15:43 +0530

    add export Card

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,3 +6,4 @@ export { Textarea } from "./textarea";
 export * as DropdownMenu from "./dropdown-menu";
 export * as Avatar from "./avatar";
 export * as Command from "./command";
+export * as Card from "./card";
