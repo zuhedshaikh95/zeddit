aa06974d8ddc87fa8af9bb2d5f28c4620c1fdedd
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-18 22:13:37 +0530

    feat: search communities

--- src/app/api/v1/search/route.ts
+++ src/app/api/v1/search/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest) {
     const results = await db.subZeddit.findMany({
       where: {
         name: {
-          startsWith: q,
+          contains: q,
         },
       },
       include: {
