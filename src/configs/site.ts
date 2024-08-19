78ab70d0ae101b0c1daaf62a45f8efa188b42187
zuhedshaikh95 <zuhed95@gmail.com>
2024-08-19 20:50:59 +0530

    add siteConfig

---
+++ src/configs/site.ts
@@ -0,0 +1,19 @@
+import { SiteConfig } from "@types";
+
+export const siteConfig: SiteConfig = {
+  name: "Zeddit - Post, Share, React",
+  handle: "@zuhedshaikh95",
+  description:
+    "Zeddit is a anonimity first social media platform that allow folks do posts and react on stuff, built using Next.js, TypeScript, Redis and TailwindCSS",
+  url: "https://zeddit-zuhed.vercel.app",
+  ogImage: "/zeddit-og.png",
+  links: {
+    mail: "zuhed95.dev@gmail.com",
+    twitter: "https://twitter.com/zuhedshaikh95",
+    github: "https://github.com/zuhedshaikh95/zeddit",
+  },
+  author: {
+    name: "Zuhed Shaikh",
+    email: "zuhed95.dev@gmail.com",
+  },
+};
